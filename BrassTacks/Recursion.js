11453a2177352bf6e4b0fffd356b72fbb12edf6b
Zeus <zeusofolympus1000@gmail.com>
2022-08-06 00:56:41 -0400

    from is a function that returns a destructured object as a tree

--- BrassTacks/Recursion.js
+++ BrassTacks/Recursion.js
@@ -5,27 +5,16 @@
 //as opposed to coming with custom solutions in an arbitrary way, we can limit the number of ways in which
 //you can generate the solution, and use a general set of tools to do it the same way every time
 class Recursion{
-    constructor(recursionObj){
-        
+    constructor(transformation){
+
     }
 
-    to(schema){
-        return obj
-    }
+    transform(transformation){
 
-    from(schema){
-        //base case, or whatever
-    }
-
-    transform(schema){
         return transform
     }
 
-    //arbitrary queries are less arbitrary when schemified in a formal way (schema is inherently recursive)
-    query(transform, schema){
-        return query
+    to(){
+        
     }
-
-
-
 }
